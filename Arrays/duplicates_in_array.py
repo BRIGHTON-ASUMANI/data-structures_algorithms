3551bf4ce3c5af6edce61e1b52e7d138add2c539
BRIGHTON-ASUMANI <asumanibrighton@gmail.com>
2021-04-30 02:24:18 +0300

    update duplicate functions

--- Arrays/duplicates_in_array.py
+++ Arrays/duplicates_in_array.py
@@ -11,10 +11,42 @@ def duplicates_in_array(arr):
     for num in arr:
         # checks if num is repeated more than once in the array
         count = freq.get(num, 0) + 1
-
         if count > 1:
             return True
 
         freq[num] = count
 
+    return False
+
+# The above funcion can be simplified to this below
+def duplicates_in_array_2(arr):
+    '''
+    This fucntion checks if an array contains duplicates using hashmaps
+    '''  
+    for num in arr:
+        if arr.count(num) > 1:
+            return True
+    return False
+
+
+
+def duplicates_in_array3(arr):
+    '''
+    This fucntion checks if an array contains duplicates by comparing list with the outcome set
+    '''
+    return len(arr) == len(set(arr))
+
+
+def duplicates_in_array_4(arr):
+    '''
+    This fucntion checks if an array contains duplicates by using sets
+    '''
+    # We can add num to the list and check if it is duplicated at the same time
+    set_nums = set()
+
+    for num in arr:
+        if num in set_nums:
+            return True
+        else:
+            set_nums.add(num)         
     return False
