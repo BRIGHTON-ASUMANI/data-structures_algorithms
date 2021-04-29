0617408b25a8b604d74ed5469c031bbd4c702052
BRIGHTON-ASUMANI <asumanibrighton@gmail.com>
2021-04-30 00:21:55 +0300

    use normal function

--- Arrays/min_in_array.py
+++ Arrays/min_in_array.py
@@ -13,3 +13,14 @@ def min_array2(arr):
     This fucntion returns the minimum number in a given array using sorted
     '''
     return sorted(arr)[0]
+
+
+def min_array3(arr):
+    '''
+    This fucntion returns the minimum number in a number assuming the first number is the smallest
+    '''
+    first_element = arr[0]  # Start by assuming the first element is smallest
+    for num in arr:       # Loop through each number in the list
+        if num < first_element:
+            first_element = num  # Update first_element if current number is less
+    return first_element
