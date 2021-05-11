5f226a0efe840ea5381c2ccb0809eb495a86585f
BRIGHTON-ASUMANI <asumanibrighton@gmail.com>
2021-05-11 13:07:06 +0300

    update function using hashmap

--- Arrays/missing_num.py
+++ Arrays/missing_num.py
@@ -1,7 +1,10 @@
-# Given an array nums containing n distinct numbers in the range [0, n],
-# return the only number in the range that is missing from the array.
+'''
+Given an array nums containing n distinct numbers in the range [0, n],
+return the only number in the range that is missing from the array.
 
-# Follow up: Could you implement a solution using only O(1) extra space complexity and O(n) runtime complexity?
+Follow up: Could you implement a solution using only O(1) extra space complexity and O(n) runtime complexity?
+'''
+
 
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
@@ -14,19 +17,19 @@ class Solution:
                 missing_value = x  # O(1)
         return missing_value  # O(n)
 
-# The time complexity is O(n) since its linear time. Only one traversal of the array is needed.
 
-# Space Complexity is O(1). No extra space is needed
+'''
+The time complexity is O(n) since its linear time. Only one traversal of the array is needed.
+Space Complexity is O(1). No extra space is needed
+'''
 
-#  If you are using your own function to get the max and number in array
-
-
+# If you are using your own function to get the max and number in array
 def min_and_max(x):
     minimum = maximum = x[0]
-    for i in x[1:]:
-        if i < minimum:
-            minimum = i
+    for w in x[1:]:
+        if w < minimum:
+            minimum = w
         else:
-            if i > maximum:
-                maximum = i
+            if w > maximum:
+                maximum = w
     return (minimum, maximum)
--- Arrays/single_number.py
+++ Arrays/single_number.py
@@ -1,5 +1,11 @@
-# Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
-# Follow up: Could you implement a solution with a linear runtime complexity and without using extra memory?
+'''
+Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+Follow up: Could you implement a solution with a linear runtime complexity and without using extra memory?
+'''
+
+
+from collections import defaultdict
+
 
 class Solution:
     def singleNumber(self, nums):
@@ -8,22 +14,34 @@ class Solution:
         :rtype: int
         """
         hash_table = {}
-        for i in nums:
+        for x in nums:
             '''
             Time complexity =  O(n * 1)=O(n).
             '''
             try:
                 # O(1) pop removes one element in an array no matter the input
-                hash_table.pop(i)
+                hash_table.pop(x)
             except:
-                hash_table[i] = 1
+                hash_table[x] = 1
         return hash_table.popitem()[0]  # constant O(1)
 
-# Time complexity =  O(n * 1)=O(n). contstans are always the same
-# Time complexity of for loop is O(n) --> increase in time according to the input(linear time)
-# Time complexity of hash table(dictionary in python) operation pop is O(1).
 
-# Space complexity : O(n). The space required by hash_table is == number of elements in nums.
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        hash_table = defaultdict(int)
+        for i in nums:
+            hash_table[i] += 1
+
+        for i in hash_table:
+            if hash_table[i] == 1:
+
+
+'''
+Time complexity =  O(n * 1)=O(n). contstans are always the same
+Time complexity of for loop is O(n) --> increase in time according to the input(linear time)
+Time complexity of hash table(dictionary in python) operation pop is O(1).
+Space complexity : O(n). The space required by hash_table is == number of elements in nums.
+'''
 
 
 class Solution:
@@ -34,5 +52,8 @@ class Solution:
         """
         return 2 * sum(set(nums)) - sum(nums)
 
-# Time complexity : O(n+n)=O(n).
-# Space complexity : O(n+n)=O(n). set needs space for the elements in
+
+'''
+Time complexity : O(n+n)=O(n).
+Space complexity : O(n+n)=O(n). set needs space for the elements in
+'''
