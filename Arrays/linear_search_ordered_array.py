62b10706d81249347caccce2790b9db091e9666b
BRIGHTON-ASUMANI <asumanibrighton@gmail.com>
2021-06-10 22:57:42 +0300

    update ordered array

--- Arrays/linear_search_ordered_array.py
+++ Arrays/linear_search_ordered_array.py
@@ -3,17 +3,21 @@
 # Normal linear search
 def linear_search(array, value):
     for i in range(len(array)):
-        if i == value:
-            return value
-        elif i > value:
-            break
-
-    return None
+        if array[i] == value:
+            return i
+    return -1
 
 
 # Ordered array linear search
 def ordered_linear_search(array, value):
-    for i in range(len(array)):
-        if array[i] == value:
-            return i
-    return -1
+    for i in array:
+        if i == value:
+            return value
+        elif i > value:
+            break
+    return None
+
+
+y = [1, 2, 3, 5, 7, 8, 20, 23, 33, 44, 55, 5555]
+
+print(ordered_linear_search(y, 55))
